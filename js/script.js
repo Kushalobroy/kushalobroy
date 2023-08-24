53fee59d49bc264ceeb46741621dcadd2638231f
Kushal <kushalobroy@gmail.com>
2023-08-24 10:42:52 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#text",{
+    strings:["Full Stack Web Developer","Web Developer","Blockchain Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
